99591b5c0d0c1f54ccadd03cce12fee37d6f5340
caiovb58-pixel <caiovb58@gmail.com>
2026-05-02 00:10:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def salvar_resultado(nome, materias, acertos, total):
         }])
         
         # Lê dados existentes e anexa o novo
-        dados_existentes = conn.read()
+        dados_existentes = conn.read(worksheet="Resultados")
         dados_atualizados = pd.concat([dados_existentes, nova_linha], ignore_index=True)
         
         conn.update(worksheet="Resultados", data=dados_atualizados)
