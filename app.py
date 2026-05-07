815962d0169818699c91e397d9a54658b5901e76
caiovb58-pixel <caiovb58@gmail.com>
2026-05-07 19:17:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -515,7 +515,7 @@ if not st.session_state.logado:
             "VMB INVEST",
             "Simulado da Ancord para SDRs que querem evoluir com método, dados e mentalidade de vencedor.",
             "rocket",
-            "SIMULADO DE ELITE"
+            "SIMULADO DA ANCORD"
         ), unsafe_allow_html=True)
         
         with st.container(border=True):
